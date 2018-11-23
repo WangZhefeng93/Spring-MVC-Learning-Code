a3b7c36d64eed555a23f4f8b6c05548914175131
WANGZHEFENG <704419968@qq.com>
2018-11-23 09:23:07 +0800

    提交成功2

--- src/com/ccff/controller/UploadController.java
+++ src/com/ccff/controller/UploadController.java
@@ -1,8 +1,15 @@
 package com.ccff.controller;
 
+import org.apache.commons.io.FileUtils;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
@@ -11,6 +18,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.io.File;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
@@ -92,5 +100,38 @@ public class UploadController {
             model.addAttribute("fileNames",fileNames);
         }
         return "upload/success";
+    }
+
+    /**
+     * 显示文件下载页面
+     * @return
+     */
+    @RequestMapping("/download")
+    public String download(){
+        return "upload/download";
+    }
+
+    /**
+     * 实现文件下载
+     * @param request
+     * @param filename
+     * @return
+     * @throws IOException
+     */
+    @RequestMapping(value = "/doDownLoad/{filename}", method = {RequestMethod.GET})
+    public ResponseEntity<byte[]> doDownLoad(HttpServletRequest request, @PathVariable("filename") String filename) throws IOException {
+        //下载文件路径
+        String path = request.getServletContext().getRealPath("/images/");
+        System.out.println(path+"\\"+filename+".jpg");
+        File file = new File(path+"\\"+filename+".jpg");
+        HttpHeaders headers = new HttpHeaders();
+        //下载显示的文件名，解决中文名称乱码问题
+        String downliadFileName = new String(filename.getBytes("UTF-8"),"iso-8859-1");
+        //通知浏览器以attachment（下载方式）打开图片
+        headers.setContentDispositionFormData("attachment",downliadFileName);
+        //application/octet-stream：二进制流数据（最常见的文件下载）
+        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+        //201 HttpStatus.CREATED
+        return new ResponseEntity<byte[]>(FileUtils.readFileToByteArray(file),headers,HttpStatus.CREATED);
     }
 }
---
+++ web/WEB-INF/jsp/upload/download.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>下载</title>
+</head>
+<body>
+    <h1>文件下载</h1>
+    <br/>
+    <a href="doDownLoad/40044eab-62a8-4363-8716-a29fc1b508c6-timg (2).jpg">反正是一张图片，你就不想下载下来看一眼吗？</a>
+</body>
+</html>
